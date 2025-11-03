9a6dd63ed26c4acaa9772405b548858d765f946a
Adasieqk <adasieqk@gmail.com>
2025-11-03 22:07:03 +0100

    Repair queries 3, 6, 12, 13

--- employee5.sql
+++ employee5.sql
@@ -11,8 +11,8 @@ NATURAL JOIN departments d;
 /* 3. Napisz zapytanie, aby znaleźć imię i nazwisko (first_name, last_name), job, department ID pracownika pracującego w London. */
 SELECT e.FIRST_NAME, e.LAST_NAME, e.JOB_ID, e.DEPARTMENT_ID
 FROM employees e
-NATURAL JOIN departments d
-NATURAL JOIN locations l
+JOIN departments d ON e.DEPARTMENT_ID = d.DEPARTMENT_ID
+JOIN locations l ON d.LOCATION_ID = l.LOCATION_ID
 WHERE l.CITY = 'London';
 
 /* 4. Napisz zapytanie, aby znaleźć employee id, last_name wraz z jego manager_id, last_name. */
@@ -32,10 +32,10 @@ LEFT JOIN employees e ON d.DEPARTMENT_ID = e.DEPARTMENT_ID
 GROUP BY d.DEPARTMENT_NAME;
 
 /* 7. Napisz zapytanie, aby znaleźć employee ID, job title, liczbę dni między datą końcową a datą początkową dla wszystkich stanowisk w dziale 90. */
-SELECT jh.EMPLOYEE_ID, jh.JOB_ID, DATEDIFF(jh.END_DATE, jh.START_DATE) AS LICZBA_DNI
-FROM job_history jh
-NATURAL JOIN employees e
-WHERE e.DEPARTMENT_ID = 90;
+SELECT EMPLOYEE_ID, JOB_TITLE, DATEDIFF(END_DATE, START_DATE) AS LICZBA_DNI
+FROM job_history
+NATURAL JOIN jobs
+WHERE DEPARTMENT_ID = 90;
 
 /* 8. Napisz zapytanie wyświetlające department ID, first_name, last_name menedżera. */
 SELECT d.DEPARTMENT_ID, m.FIRST_NAME, m.LAST_NAME
@@ -55,15 +55,16 @@ NATURAL JOIN employees e
 GROUP BY j.JOB_TITLE;
 
 /* 11. Napisz zapytanie wyświetlające job title, employee name oraz różnicę pomiędzy wynagrodzeniem pracownika a minimalnym wynagrodzeniem na danym stanowisku. */
-SELECT j.JOB_TITLE, e.FIRST_NAME (e.SALARY - j.MIN_SALARY) AS ROZNICA_PENSJI
-FROM employees e
-NATURAL JOIN jobs j;
+SELECT  JOB_TITLE, FIRST_NAME, LAST_NAME, (SALARY - MIN_SALARY) AS ROZNICA_WYNAGRODZEN
+FROM employees
+NATURAL JOIN jobs;
 
 /* 12. Napisz zapytanie wyświetlające historię stanowisk pracy wykonanych przez dowolnego pracownika pobierającego obecnie ponad 10 000 pensji. */
-SELECT eh.EMPLOYEE_ID, e.FIRST_NAME, e.LAST_NAME, eh.JOB_ID, eh.START_DATE, eh.END_DATE
-FROM job_history eh
-NATURAL JOIN employees e
-WHERE e.SALARY > 10000;
+SELECT e.EMPLOYEE_ID, e.FIRST_NAME, e.LAST_NAME, jh.JOB_ID, j.JOB_TITLE, jh.START_DATE, jh.END_DATE, jh.DEPARTMENT_ID
+FROM employees e JOIN job_history jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID JOIN jobs j ON j.JOB_ID = jh.JOB_ID
+WHERE e.SALARY > 10000
+ORDER BY e.EMPLOYEE_ID, jh.START_DATE;
+
 
 /* 13. Napisz zapytanie wyświetlające department name, first_name, last_name, hire date, salary menadżera dla wszystkich menadżerów ze stażem pracy powyżej 15 lat. */
 SELECT d.DEPARTMENT_NAME, e.FIRST_NAME, e.LAST_NAME, e.HIRE_DATE, e.SALARY, m.FIRST_NAME
